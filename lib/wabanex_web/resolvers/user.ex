fa571e81f1150cfd8d701f12631e2b27a45fb42f
João Pedro F. Dantas <fonsecajope@gmail.com>
2022-01-06 17:48:45 -0300

    Define `create` for root mutation

--- lib/wabanex_web/resolvers/user.ex
+++ lib/wabanex_web/resolvers/user.ex
@@ -1,4 +1,8 @@
 defmodule WabanexWeb.Resolvers.User do
+  def create(%{input: params}, _context) do
+    Wabanex.Users.Create.call(params)
+  end
+
   def get(%{id: user_id}, _context) do
     Wabanex.Users.Get.call(user_id)
   end

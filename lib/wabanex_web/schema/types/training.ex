10cd40aac9584149246fefa762fdf32b843566ab
João Pedro F. Dantas <fonsecajope@gmail.com>
2022-01-13 08:55:44 -0300

    Fix names of object and exercises fields of training

--- lib/wabanex_web/schema/types/training.ex
+++ lib/wabanex_web/schema/types/training.ex
@@ -4,17 +4,17 @@ defmodule WabanexWeb.Schema.Types.Training do
   import_types WabanexWeb.Schema.Types.Exercise
 
   @desc "Logic training representation"
-  object :user do
+  object :training do
     field :id, non_null(:uuid4)
     field :start_date, non_null(:string)
     field :end_date, non_null(:string)
-    field :exercises, list_of(:exercises)
+    field :exercises, list_of(:exercise)
   end
 
   input_object :create_training_input do
     field :user_id, non_null(:uuid4), description: "Trainings name"
     field :start_date, non_null(:string), description: "Trainings start date"
     field :end_date, non_null(:string), description: "Trainings end date"
-    field :exercises, list_of(:exercise_input), description: "Trainings exercises"
+    field :exercises, list_of(:create_exercise_input), description: "Trainings exercises"
   end
 end
